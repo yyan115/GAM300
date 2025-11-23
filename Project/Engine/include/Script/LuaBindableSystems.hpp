9db7f8b9f9d0ccc150de5a26a5cc15bfc28d6c70
Austin Ang <a.jiajunaustin@gmail.com>
2025-11-24 02:10:03 +0800

    ignore this

--- Project/Engine/include/Script/LuaBindableSystems.hpp
+++ Project/Engine/include/Script/LuaBindableSystems.hpp
@@ -21,3 +21,57 @@ namespace InputWrappers {
         return InputManager::GetMouseButtonDown(static_cast<Input::MouseButton>(button));
     }
 }
+
+
+namespace CharacterControllerWrappers {
+
+    // Constructor wrapper
+    inline CharacterController* Create(JPH::PhysicsSystem* physicsSystem) {
+        return new CharacterController(physicsSystem);
+    }
+
+    // Movement wrappers
+    inline void Move(CharacterController* controller, float x, float y, float z) {
+        if (controller)
+            controller->Move(x, y, z);
+    }
+
+    inline void Jump(CharacterController* controller, float height) {
+        if (controller)
+            controller->Jump(height);
+    }
+
+    inline void Update(CharacterController* controller, float deltaTime) {
+        if (controller)
+            controller->Update(deltaTime);
+    }
+
+    inline JPH::Vec3 GetPosition(CharacterController* controller) {
+        return controller ? controller->GetPosition() : JPH::Vec3::sZero();
+    }
+
+    inline void SetVelocity(CharacterController* controller, const JPH::Vec3& vel) {
+        if (controller)
+            controller->SetVelocity(vel);
+    }
+
+    inline JPH::Vec3 GetVelocity(CharacterController* controller) {
+        return controller ? controller->GetVelocity() : JPH::Vec3::sZero();
+    }
+
+}
+
+#include "Physics/PhysicsSystem.hpp"
+#include "ECS/ECSRegistry.hpp"
+
+namespace PhysicsSystemWrappers {
+    inline JPH::PhysicsSystem* GetSystem() {
+        // Get active ECSManager as reference, then take address of physicsSystem
+        ECSManager& ecsManager = ECSRegistry::GetInstance().GetActiveECSManager();
+
+        if (!ecsManager.physicsSystem)
+            return nullptr;
+
+        return &ecsManager.physicsSystem->GetJoltSystem();
+    }
+}
--- Project/Resources/Scripts/template/CharacterTest.lua
+++ Project/Resources/Scripts/template/CharacterTest.lua
@@ -1,24 +1,58 @@
--- rigidbody_test.lua
-require("extension.engine_bootstrap")
-local Component = require("extension.mono_helper")
-local debug = require("extension.debug_helpers")
+local Component = require("mono_helper")
 
 return Component {
-    fields = {},  -- optional if your engine requires it
+    fields = {
+        name = "CharacterControllerTest",
+        physicsSystem = nil,    -- to store the physics system reference
+        controller = nil        -- to store the CharacterController instance
+    },
+
+    Awake = function(self)
+        print("CharacterControllerTest Awake - Getting Physics System")
+        -- Get PhysicsSystem from engine 
+        self.physicsSystem = PhysicsSystem.GetSystem()
+        if self.physicsSystem == nil then
+            print("ERROR: PhysicsSystem not available!")
+            return
+        end
+    end,
 
     Start = function(self)
-        -- create an instance field _transform
-        self._cc = self:GetComponent("CharacterControllerComponent")
-        self._cc.speed = 10
+        print("CharacterControllerTest Start - Creating CharacterController")
+        self.controller = CharacterController.new(self.physicsSystem)
+        if self.controller == nil then
+            print("ERROR: Failed to create CharacterController!")
+            return
+        end
+        print("CharacterController created successfully!")
     end,
 
     Update = function(self, dt)
+        if self.controller == nil then return end
 
-        if self._cc then
-            print("CharacterComponent is obtained")
-        else
-            print("shit, this aint working...")
+        -- Example movement: move forward continuously
+        self.controller:Move(1, 0, 0)
+        print("Controller moved along X by speed*dt")
+
+        -- Example jump (trigger once)
+        if Input.GetKeyDown(Input.Key.Space) then
+            self.controller:Jump(5)  -- jump height = 5
+            print("Controller jumped!")
         end
 
+        -- Update controller for this frame
+        self.controller:Update(dt)
+
+        -- Print position if GetPosition wrapper exists
+        if self.controller.GetPosition then
+            local pos = self.controller:GetPosition()
+            print("Current position: x=" .. pos.x .. " y=" .. pos.y .. " z=" .. pos.z)
+        end
     end,
+
+    OnDisable = function(self)
+        print("CharacterControllerTest disabled")
+        -- Optionally destroy controller later if Destroy wrapper exists
+        -- CharacterController.Destroy(self.controller)
+    end
 }
