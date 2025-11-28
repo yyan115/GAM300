5d63f47e9ca68cf3b45997da6c4664edf2e838dd
Austin Ang <a.jiajunaustin@gmail.com>
2025-11-29 02:40:53 +0800

    added enemy logic

--- Project/Resources/Scripts/Gameplay/EnemyLogic.lua
+++ Project/Resources/Scripts/Gameplay/EnemyLogic.lua
@@ -1,36 +1,90 @@
--- simple_mover.lua
--- Moves an entity in a direction at a given speed
--- This is what a non-programmer would write!
-
 require("extension.engine_bootstrap")
 local Component = require("extension.mono_helper")
 local TransformMixin = require("extension.transform_mixin")
 
+-- Animation States
+local IDLE          = 0
+local ATTACK        = 1
+local TAKE_DAMAGE   = 2
+local DEATH         = 3
+
+local playerNear = false
+local currentState = IDLE
+
+-- Helper Functions
+local function IsPlayerInRange()
+    if Input.GetKeyDown(Input.Key.U) then
+        playerNear = true
+    end
+    return playerNear
+end
+
+local function TakeDamage(self)
+    self.Health = self.Health - 1
+end
+
 return Component {
-    -- Apply the transform mixin for easy movement
     mixins = { TransformMixin },
     
-    -- Editable fields (visible in inspector)
     fields = {
-        speed = 2.0,
-        directionX = 1.0,
-        directionY = 0.0,
-        directionZ = 0.0
+        Health          = 5,
+        Damage          = 1,
+        Attack_Speed    = 1.0,
+        Attack_Cooldown = 1.0
     },
     
-    -- Called once when the component is created
-    Start = function(self)
-        print("SimpleMover started with speed: " .. tostring(self.speed))
+    Start = function(self) 
+        self.animation = self:GetComponent("AnimationComponent") 
+        self.collider = self:GetComponent("ColliderComponent")
+        Animation.PlayClip(self.animation, IDLE, true)
+        currentState = IDLE
     end,
     
-    -- Called every frame
-    Update = function(self, dt)
-        -- Calculate movement delta
-        local dx = self.directionX * self.speed * dt
-        local dy = self.directionY * self.speed * dt
-        local dz = self.directionZ * self.speed * dt
+    Update = function(self, dt) 
+        local newState = currentState
+
+        -- Determine new state 
+        if self.Health <= 0 then
+            newState = DEATH
+        elseif Input.GetKeyDown(Input.Key.U) then
+            TakeDamage(self)
+            newState = TAKE_DAMAGE
+        elseif Input.GetKeyDown(Input.Key.I) then
+            newState = ATTACK
+        else
+            newState = IDLE
+        end
         
-        -- Move the entity (handles all the complexity internally)
-        self:Move(dx, dy, dz)
+        -- Apply state transition rules
+        if currentState == DEATH then
+            newState = DEATH  -- Death cannot be interrupted
+        elseif currentState == TAKE_DAMAGE then
+            if Animation.IsPlaying(self.animation) then
+                newState = TAKE_DAMAGE  -- Keep playing damage animation
+            else
+                newState = ATTACK  -- After damage, go to attack
+            end
+        elseif currentState == ATTACK and newState == IDLE then
+            newState = ATTACK  -- Attack cannot be interrupted by idle
+        end
+        
+        -- Update animation if state changed
+        if newState ~= currentState then
+            Animation.Pause(self.animation)
+            local loop = (newState ~= DEATH and newState ~= TAKE_DAMAGE)
+            Animation.PlayClip(self.animation, newState, loop)
+            currentState = newState
+        end
     end
 }
+
+--  1 = ATTACK
+--  2 = TAKEDAMAGE
+--  3 = ENEMYDEATH
+
+-- Animation.PlayOnce(self.animation, AttackAnimation)      -- plays a clip once
+-- Animation.Pause(self.animation)                           -- pauses playback
+-- Animation.Stop(self.animation)                            -- stops and resets playback
+-- Animation.SetSpeed(self.animation, 1.5)                  -- sets playback speed
+-- Animation.SetLooping(self.animation, true)               -- sets looping
+-- local playing = Animation.IsPlaying(self.animation)      -- returns true/false
