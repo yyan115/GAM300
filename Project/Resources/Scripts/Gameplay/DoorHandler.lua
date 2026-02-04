0bb73cf392b122d4946fb42828908f90ff8f57e9
PELIX\nce23 <pelix>
2026-02-04 21:30:43 +0800

    Broken doorhandle.lua

--- Project/Resources/Scripts/Gameplay/DoorHandler.lua
+++ Project/Resources/Scripts/Gameplay/DoorHandler.lua
@@ -1,22 +1,27 @@
 require("extension.engine_bootstrap")
 local Component = require("extension.mono_helper")
-local TransformMixin = require("extension.transform_mixin")
 
 local event_bus = _G.event_bus
-local Physics = _G.Physics
+local Engine = _G.Engine
+
+local function distSqXZ(ax, az, bx, bz)
+    local dx = ax - bx
+    local dz = az - bz
+    return dx*dx + dz*dz
+end
 
 return Component {
-    mixins = { TransformMixin },
 
     fields = {
-        Radius = 1.0
+        Radius = 1.2
     },
 
-    Awake = function(self)
-        self._playerPos = nil
-        self._inside = false
+    Start = function(self)
+        print("[PedestalTrigger] Start. entityId =", self.entityId)
 
-        -- already provided by your PlayerMovement script
+        self._inside = false
+        self._playerPos = nil
+
         if event_bus and event_bus.subscribe then
             self._sub = event_bus.subscribe("player_position", function(pos)
                 self._playerPos = pos
@@ -26,21 +31,33 @@ return Component {
 
     Update = function(self, dt)
         if not self._playerPos then return end
-        if not Physics or not Physics.CheckDistance then return end
+        if not Engine or not Engine.GetEntityPosition then return end
 
-        local p = self:GetComponent("Transform").localPosition
+        -- pedestal world position (THIS IS THE IMPORTANT LINE)
+        local pedPos = Engine.GetEntityPosition(self.entityId)
+        if not pedPos then
+            return
+        end
 
-        -- CheckDistance(x1,y1,z1, x2,y2,z2, radius)
-        local inside = Physics.CheckDistance(
-            self._playerPos.x, self._playerPos.y, self._playerPos.z,
-            p.x, p.y, p.z,
-            self.Radius
-        )
+        local px = self._playerPos.x
+        local pz = self._playerPos.z
+
+        local tx = pedPos.x
+        local tz = pedPos.z
+
+        local r = self.Radius
+        local inside = distSqXZ(px, pz, tx, tz) <= (r * r)
 
         if inside and not self._inside then
-            print("Player collided with trigger!")
+            print("Player collided with pedestal")
         end
 
         self._inside = inside
+    end,
+
+    OnDisable = function(self)
+        if event_bus and event_bus.unsubscribe and self._sub then
+            event_bus.unsubscribe(self._sub)
+        end
     end,
 }
