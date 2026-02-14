a4559c527f44aac4ec2ea6c2a14491d0fdc25f87
sTsenre <h.yonghengernest@digipen.edu>
2026-02-15 00:30:48 +0800

    Added Blending variables

--- Project/Engine/include/Animation/Animator.hpp
+++ Project/Engine/include/Animation/Animator.hpp
@@ -13,8 +13,12 @@ public:
 	void UpdateAnimation(float dt, bool isLoop, Entity entity);
 
 	void PlayAnimation(Animation* pAnimation, Entity entity);
-	void ClearAnimation() { mCurrentAnimation = nullptr; }
+	void ClearAnimation() { mCurrentAnimation = nullptr; mIsBlending = false; mPrevAnimation = nullptr; }
 	bool HasAnimation() const { return mCurrentAnimation != nullptr; }
+
+	// Crossfade blending
+	void StartCrossfade(Animation* newAnim, float duration, bool prevLoop, Entity entity);
+	bool IsBlending() const { return mIsBlending; }
 
 	void CalculateBoneTransform(const AssimpNodeData* node, glm::mat4 parentTransform, Entity entity, bool bakeParent = false);
 	void CalculateBoneTransformInternal(const AssimpNodeData* node, glm::mat4 parentTransform, Entity entity, bool bakeParent,
@@ -25,6 +29,18 @@ public:
 	void SetCurrentTime(float time, Entity entity); // For editor preview
 
 private:
+	void CalculateBlendedBoneTransform(const AssimpNodeData* node, glm::mat4 parentTransform, Entity entity, bool bakeParent, float blendFactor);
+	void CalculateBlendedBoneTransformInternal(const AssimpNodeData* node, glm::mat4 parentTransform, Entity entity, bool bakeParent,
+		ECSManager& ecsManager, const std::map<std::string, BoneInfo>& boneInfoMap, const glm::mat4& globalInverse, float blendFactor);
+
 	Animation* mCurrentAnimation = nullptr;
 	float mCurrentTime = 0.0f;
+
+	// Crossfade blending state
+	Animation* mPrevAnimation = nullptr;
+	float mPrevTime = 0.0f;
+	float mBlendDuration = 0.0f;
+	float mBlendElapsed = 0.0f;
+	bool mIsBlending = false;
+	bool mPrevIsLoop = false;
 };
