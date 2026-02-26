e836bd11c82416148f3d739117d40dbc9f18a865
Woon Li <zack.woonli@gmail.com>
2026-02-26 23:25:03 +0800

    optimize EnemyAI.lua a bit

--- Project/Resources/Scripts/Gameplay/EnemyAI.lua
+++ Project/Resources/Scripts/Gameplay/EnemyAI.lua
@@ -517,12 +517,6 @@ return Component {
                 x = x, y = y, z = z
             })
         end
-
-        -- [CRITICAL ADDITION] Ensure the scheduler actually runs!
-        -- If your engine doesn't call _G.update(dt) automatically, the feathers will never spawn.
-        if _G.scheduler and _G.scheduler.tick then
-             _G.scheduler.tick(dt)
-        end
     end,
 
     GetRanges = function(self)
