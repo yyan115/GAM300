e3b3d18352139e2ab9ac5553dea494e4b7837fd6
yy <yyan115@outlook.com>
2026-04-02 00:04:39 +0800

    add macro for enabling/disabling console for game builds, remove installer

--- Project/cmake/CopyResources.cmake
+++ Project/cmake/CopyResources.cmake
@@ -1,10 +1,3 @@
 # Copies the Resources directory to the destination, excluding source/editor-only files.
 # Usage: cmake -DSRC=<src> -DDST=<dst> -P CopyResources.cmake
-file(COPY "${SRC}/" DESTINATION "${DST}"
-    PATTERN "*.png"  EXCLUDE
-    PATTERN "*.PNG"  EXCLUDE
-    PATTERN "*.fbx"  EXCLUDE
-    PATTERN "*.FBX"  EXCLUDE
-    PATTERN "*.obj"  EXCLUDE
-    PATTERN "*.meta" EXCLUDE
-)
+file(COPY "${SRC}/" DESTINATION "${DST}")
